722b888cd5323d033c1548f97a606d15fb15a66a
lzl0125 <lianglun0125@gmail.com>
2022-12-05 01:56:51 +0800

    Add files via upload

---
+++ CalculatorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+ 
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate(1,5);
+    assertEquals(5, sum);
+  }
+}
