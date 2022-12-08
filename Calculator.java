7c80dcb01d13ce95ea7ab535ff08175f13b2e3b3
lzl0125 <lianglun0125@gmail.com>
2022-12-08 19:57:36 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public int subtract(int x,int y){
         return x-y;
     }
-    public int multiplicat(int x,int y){
+    public int multiplication(int x,int y){
         return x*y;
     }
     public int division(int x,int y){
