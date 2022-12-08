6a834b5271cd679e455e254b22b273842f54cf97
lzl0125 <lianglun0125@gmail.com>
2022-12-08 19:54:48 +0800

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -8,19 +8,22 @@ public class CalculatorTest {
     int sum = calculator.add(1,5);
     assertEquals(6, sum);
   }
+  @Test
   public void testSub() {
     Calculator calculator = new Calculator();
     int sum = calculator.subtract(13213,99);
     assertEquals(13114, sum);
   }
+  @Test
   public void testMult() {
     Calculator calculator = new Calculator();
     int sum = calculator.multiplicat(456,99);
     assertEquals(45144, sum);
   }
-  public void testDiv() {
+  @Test
+  public void testSub() {
     Calculator calculator = new Calculator();
-    int sum = calculator.division(12345,15);
+    int sum = calculator.subtract(12345,15);
     assertEquals(823, sum);
   }
 }
